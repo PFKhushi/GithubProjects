96974a4eb645db6baa18d544e5801bfb54e790c9
Pedro Firmino <mafiazuka@gmail.com>
2024-03-27 05:30:44 -0300

    Adicionado prototipo da solucao em java para o problema

---
+++ Faculdade/5 Periodo/Teoria dos Grafos/Graph Paraiba State/JAVA/StateGraph.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class StateGraph {
+
+    public class Cidade {
+        String nome;
+        int ID;
+        Vector<Integer> conexoes; // Recebe os IDs das cidades com as quais tem conexao
+    }
+
+//////////////////////////////////////////////////////
+
+    public static void main (String[] args) {
+
+        Vector<Integer> matrix = new Vector<>(); // É um vetor 2D que segue a formula [coluna * num_elementos + linha]
+
+
+    }
+//////////////////////////////////////////////////////
+    public static Vector<Integer> fazMatrix(ArrayList<Cidade> cidades){
+
+        Vector<Integer> nova_matrix = new Vector<>();
+
+        return nova_matrix;
+    }
+}
