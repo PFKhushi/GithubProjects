cd1d083f87ee446cbe2c09b0ea5a978a77be3eae
Pedro Firmino <mafiazuka@gmail.com>
2024-03-26 22:25:01 -0300

    Funcao para retirar a cor da imagem com a media do pixel adicionada

--- Faculdade/5 Periodo/Processamento de Imagem/Primeiro Manipulador de Imagem/JAVA/src/LeitorImagem.java
+++ Faculdade/5 Periodo/Processamento de Imagem/Primeiro Manipulador de Imagem/JAVA/src/LeitorImagem.java
@@ -11,29 +11,70 @@ import java.io.IOException;
 public class LeitorImagem{
 
     public static void main (String[] args) {
+
+        File file = new File("../../DATOS/Originais/image.png");
+        BufferedImage imagem = null;
+        BufferedImage nova_imagem = null;
+
         try {
-            ///File imagem = new File("Primeiro Manipulador de Imagem/DATOS/Originais/image.png");
-            BufferedImage image = ImageIO.read(new File("../../DATOS/Originais/image.png"));
+            imagem = ImageIO.read(file);
 
-            // Create a JLabel to hold the image icon
-            JLabel imageLabel = new JLabel(new ImageIcon(image));
-
-            // Create a JFrame to display the image
-            JFrame frame = new JFrame("Image Display");
-            frame.add(imageLabel);
-            frame.pack();
-            frame.setVisible(true);
-
-        } catch(Exception e){
-            e.printStackTrace();
+        } catch(IOException e){
+            e.printStackTrace(System.out);
         }
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        }
-        System.exit(0);
 
+        if (imagem != null){
+           /// display(imagem);
+            pretoEbranco(imagem);
+            display(imagem);
+        }
+
+    }
+    public static void display (BufferedImage imagem) {
+        System.out.println("Displaying image");
+        JFrame frame = new JFrame();
+        JLabel label = new JLabel();
+        frame.setSize(imagem.getWidth(), imagem.getHeight());
+        label.setIcon(new ImageIcon(imagem));
+        frame.getContentPane().add(label, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+    public static void pretoEbranco (BufferedImage imagem){
+
+        int p, a, r, g, b, md;
+
+        for(int l = 0; l < imagem.getHeight(); l++){
+            for(int c = 0; c < imagem.getWidth(); c++){
+
+                System.out.println(1);
+                p = imagem.getRGB(c, l);
+                System.out.println(2);
+                a = (p>>24)&0xff;
+                System.out.println(3);
+                r = (p>>16)&0xff;
+                System.out.println(4);
+                g = (p>>8)&0xff;
+                System.out.println(5);
+                b = p&0xff;
+                System.out.println(6);
+
+                md = (r+g+b)/3;
+                System.out.println(7);
+
+                p = (a<<24) | (md<<16) | (md<<8) | md;
+                System.out.println(8);
+
+                imagem.setRGB(c, l, p);
+                System.out.println("\n\nLinha: " + l + " Coluna: " + c);
+                System.out.println("Height " + imagem.getHeight() + " Width " + imagem.getWidth());
+            }
+        }
+
+        display(imagem);
 
     }
 }
